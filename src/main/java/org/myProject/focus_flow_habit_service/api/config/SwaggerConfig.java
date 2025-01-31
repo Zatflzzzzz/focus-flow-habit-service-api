700bf123bdeebc3dbc6bcc84367929ec1b67c739
zatflz <timofeysangjey@gmail.com>
2025-01-31 22:07:29 +0300

    I have completed the implementation of docker in my project, and also added swagger for the documentation api.

--- src/main/java/org/myProject/focus_flow_habit_service/api/config/SwaggerConfig.java
+++ src/main/java/org/myProject/focus_flow_habit_service/api/config/SwaggerConfig.java
@@ -1,4 +1,29 @@
 package org.myProject.focus_flow_habit_service.api.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8184")
+                        )
+                )
+                .info(
+                        new Info()
+                                .title("My API")
+                                .description("API documentation")
+                                .version("1.0.0")
+                );
+    }
 }
+
