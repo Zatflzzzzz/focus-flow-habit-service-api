41379856bde6c060d73085c462161cae47ffcd9a
zatflz <timofeysangjey@gmail.com>
2025-02-05 18:19:51 +0300

    I finished adding the docker compiler, contacted spring gateway, and changed the swagger documentation for the user interface

--- src/main/java/org/myProject/focus_flow_habit_service/api/config/SwaggerConfig.java
+++ src/main/java/org/myProject/focus_flow_habit_service/api/config/SwaggerConfig.java
@@ -1,28 +1,30 @@
 package org.myProject.focus_flow_habit_service.api.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .servers(
-                        List.of(
+                .servers(List.of(
                                 new Server().url("http://localhost:8184")
                         )
                 )
-                .info(
-                        new Info()
-                                .title("My API")
-                                .description("API documentation")
-                                .version("1.0.0")
+                .info(new Info()
+                                .title("Focus Flow Habit Api")
+                                .description("This is the Rest Api for Habit Service")
+                                .version("v0.0.1")
+                                .license(new License().name("Apache 2.0"))
                 );
     }
 }
